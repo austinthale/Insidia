cda08019260b2befb412fa55d4247fd2926b4ab2
Unknown <hopskipbye@gmail.com>
2017-10-03 14:15:43 -0600

    Minion AI

---
+++ AI/Minion_AI_KMotter/ActivateMinion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class is designed to be attached to an object with a trigger collider that will
+///     only react to a PC (generally done though physics layers).
+///     
+/// It requires an associated 'minion' prefab (stored as Transform) that it activates
+///     (using Activate() within the MinionNavigation class) upon being triggered.
+///     
+/// This script disables its object upon being triggered.
+/// </summary>
+public class ActivateMinion : MonoBehaviour {
+
+    public Transform minion;
+    MinionNavigation ai;
+
+	// Use this for initialization
+	void Start () {
+        ai = minion.GetComponent<MinionNavigation>();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ai.owningPlayer = other.transform;
+        ai.Activate();
+        gameObject.SetActive(false);
+    }
+}
---
+++ AI/Minion_AI_KMotter/MinionNavigation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/// <summary>
+/// This class is designed to control Minion navigation. It requires an attached NavMesh Agent.
+/// </summary>
+public class MinionNavigation : MonoBehaviour {
+
+    public Transform owningPlayer;
+
+    private NavMeshAgent agent;
+    private float updateDestinationDelay = .25f;
+
+    Coroutine follow;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+
+    /// <summary>
+    /// This is a coroutine that keeps this minion following its player.
+    /// </summary>
+    /// <param name="_player"> The player </param>
+    /// <returns></returns>
+    IEnumerator Follow(Transform _player)
+    {
+        while (true)
+        {
+            agent.SetDestination(owningPlayer.position);
+            yield return new WaitForSeconds(updateDestinationDelay);
+        }
+    }
+
+    /// <summary>
+    /// This function starts a Follow() coroutine and stores it in a variable for further control.
+    /// </summary>
+    public void Activate()
+    {
+        follow = StartCoroutine(Follow(owningPlayer));
+    }
+}
